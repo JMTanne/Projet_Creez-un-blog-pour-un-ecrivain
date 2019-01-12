d55d63518aa245a7ef242f05f136d2eca213561e
JMTanne <46629500+JMTanne@users.noreply.github.com>
2019-01-12 18:24:54 +0100

    Preparation of the CommentManager for next requests

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -7,40 +7,5 @@ require_once("model/Manager.php");
 
 class CommentManager extends Manager
 {
-    public function getComments($postId)
-    {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-        $comments->execute(array($postId));
 
-        return $comments;
-    }
-
-    public function postComment($postId, $author, $comment)
-    {
-        $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        $affectedLines = $comments->execute(array($postId, $author, $comment));
-
-        return $affectedLines;
-    }
-    // Fonction public membre, permettant la récupération d'un commentaire pour modification
-    public function getComment($commentId)
-    {
-        $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id = ?');
-        $req->execute(array($commentId));
-        $comment = $req->fetch();
-
-        return $comment;
-    }
-    // Fonction permettant la modification du commentaire
-    public function updateComment($comment, $commentId)
-    {
-        $db = $this->dbConnect();
-        $req = $db->prepare('UPDATE comments SET comment = ?, comment_date = NOW() WHERE id = ?');
-        $newLine = $req->execute(array($comment, $commentId));
-
-        return $newLine;
-    }
 }
