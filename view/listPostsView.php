2ae49ca8f8dad20f8166a03ba1830603000f5f0b
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 11:40:05 +0100

    Add the 'Comments' link below all Posts/Chapters containers. Add function to see only the fists 1000 string caracters of a Post and a Link to see All Post/Chapter

--- view/listPostsView.php
+++ view/listPostsView.php
@@ -3,14 +3,14 @@
 <?php ob_start(); ?>
 
 <p>
-    <a href="index.php">Retour à la liste des billets</a>
+    <a class="back_link" href="index.php">Retour à l'Accueil</a>
 </p>
 
 <div class="index_pages">
 
 <section id="listPosts_index">
 
-    <h1>Liste des chapitres:</h1>
+    <h1>Liste des chapitres</h1>
     
         <p> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
 
@@ -29,9 +29,13 @@
                 </h3>
             
                     <p>
-                    <?= nl2br(htmlspecialchars($data['post_content'])) ?>
+                    <?= nl2br(htmlspecialchars(substr($data['post_content'], 0, 1000))) . '... ' ?> <em><a class="all_chapter" href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire le chapitre entier</a></em>
+                    </p>
+                    <p>
+                    <em><a class="comments" href="index.php?action=comments&amp;id=<?= $data['id'] ?>">[Commentaires]</a></em><em>-</em><em><a class="alert" href="">[Signaler]</a></em>
                     <br />
-                </p>
+                    
+                    </p>
             </div>
         </div>
     <?php
