c97fc3b0f136c252a29c1d54e51e0f6b82c42a9f
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 19:13:37 +0100

    Update Router with Back Office actions cases and Controller functions : allAlerts(), deleteAlert(), addPost(), newPost(), BO_allPosts() & BO_post()

--- index.php
+++ index.php
@@ -3,14 +3,17 @@ session_start();
 // Création d'un namespace
 use \P4\Projet\Controller\ControllerPosts;
 use \P4\Projet\Controller\ControllerComments;
+use \P4\Projet\Controller\ControllerAlerts;
 
 // Chargement des classes
 require('controller/ControllerPosts.php');
 require('controller/ControllerComments.php');
+require('controller/ControllerAlerts.php');
 
 // Création d'un objet "Controller"
 $initControllerPosts = new ControllerPosts;
 $initControllerComments = new ControllerComments;
+$initControllerAlerts = new ControllerAlerts;
 
 try {
     // Mise en place du routeur:
@@ -30,7 +33,7 @@ try {
                 $initControllerComments->newComment($_GET['id'], $_POST['comment_author'], $_POST['comment_content']);
                 break;
             case "alertComment":
-                $initControllerComments->alertComment($_GET['id'], $_POST['comment_author'], $_POST['comment_content']);
+                $initControllerAlerts->alert($_GET['commentId'], $_GET['commentPostId'], $_GET['commentAuthor'], $_GET['commentContent'], $_GET['postId']);
                 break;
             case "commentDeleted":
                 $initControllerComments->deleteComment($_GET['id'], $_GET['postId']);
@@ -41,15 +44,27 @@ try {
 
             // Ajout des pages "Back Office":
             case "BO_welcome":
-                echo "Redirection vers la page 'welcome Back Office'";
+                $initControllerAlerts->allAlerts();
                 break;
-            case "BO_allPosts":
-                echo "Redirection vers la page 'allPosts Back Office'";
+            case "BO_deleteAlert":
+                $initControllerAlerts->deleteAlert($_GET['alertId']);
                 break;
             case "BO_addPost":
-                echo "Redirection vers la page 'addPost Back Office'";
+                $initControllerPosts->addPost();
+                break;
+            case "BO_postAdded":
+                $initControllerPosts->newPost($_POST['chapter_id'], $_POST['post_title'], $_POST['post_content']);
+                break;
+            case "BO_allPosts":
+                $initControllerPosts->BO_allPosts();
+                break;
+            case "BO_post":
+                $initControllerPosts->BO_post($_GET['id']);
                 break;
             case "BO_modifPost":
+                echo "Redirection vers la page 'BO_modifPost Back Office'";
+                break;
+            case "BO_deletePost":
                 echo "Redirection vers la page 'BO_modifPost Back Office'";
                 break;
             default:
