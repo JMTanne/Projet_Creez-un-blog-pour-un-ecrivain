adf0428dd2b5e276efca6e5ae64355b4c4c4c1d7
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 14:16:28 +0100

    Update Router cases for new controller functions : addComment, alertComment and commentDeleted

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-require('controller/controller.php');
+session_start();
+// Création d'un namespace
+use \P4\Projet\Controller\ControllerPosts;
+use \P4\Projet\Controller\ControllerComments;
+
+// Chargement des classes
+require('controller/ControllerPosts.php');
+require('controller/ControllerComments.php');
+
+// Création d'un objet "Controller"
+$initControllerPosts = new ControllerPosts;
+$initControllerComments = new ControllerComments;
 
 try {
     // Mise en place du routeur:
@@ -7,19 +18,22 @@ try {
         switch ($_GET['action']) {
             // Ajout des pages "Front":
             case "allPosts":
-                allPosts();
+                $initControllerPosts->allPosts();
                 break;
             case "post":
-                post();
+                $initControllerPosts->post($_GET['id']);
                 break;
             case "comments":
-                comments();
+                $initControllerComments->comments($_GET['id'], $_GET['id']);
                 break;
             case "addComment":
-                echo "Redirection vers la page 'addComment'";
+                $initControllerComments->newComment($_GET['id'], $_POST['comment_author'], $_POST['comment_content']);
                 break;
-            case "updateComment":
-                echo "Redirection vers la page 'updateComment'";
+            case "alertComment":
+                $initControllerComments->alertComment($_GET['id'], $_POST['comment_author'], $_POST['comment_content']);
+                break;
+            case "commentDeleted":
+                $initControllerComments->deleteComment($_GET['id'], $_GET['postId']);
                 break;
             case "registrationLogin":
                 echo "Redirection vers la page 'login";
@@ -39,10 +53,10 @@ try {
                 echo "Redirection vers la page 'BO_modifPost Back Office'";
                 break;
             default:
-            lastPost();
+            $initControllerPosts->lastPost();
         }
     } else {
-        lastPost();
+        $initControllerPosts->lastPost();
     }
 }
 catch(Exception $e) {
