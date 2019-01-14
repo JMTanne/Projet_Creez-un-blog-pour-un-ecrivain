d402ff862e34f0f62de5719b80773c7be06759b6
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 11:42:56 +0100

    Add the 'Comments' link below the Last Post present on home + Add function to see only the fists 1000 string caracters of the Last Post and a Link to see All Last Post/Chapter

--- view/welcomeView.php
+++ view/welcomeView.php
@@ -6,13 +6,11 @@
 
 	<section id="welcome_index">
 		
-		<h1>Bienvenue sur mon Blog !</h1>
+		<h1>Bienvenue sur mon Blog</h1>
 	
 		<p> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
 
 	</section>
-
-	<div id="welcome_section">
 
 		<section id="welcome_lastPosts">
 
@@ -28,8 +26,11 @@
 		            	<em>ajouté le <?= $data['creation_date_fr'] ?></em>
 		        	</h3>
 		        
-			        	<p>
-			            <?= nl2br(htmlspecialchars($data['post_content'])) ?>
+			        <p>
+			            <?= nl2br(htmlspecialchars(substr($data['post_content'], 0, 1000))) . '... ' ?> <em><a class="all_chapter" href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire le chapitre entier</a></em>
+			        </p>
+			        <p>
+			            <em><a class="comments" href="index.php?action=comments&amp;id=<?= $data['id'] ?>">[Commentaires]</a></em><em>-</em><em><a class="alert" href="#">[Signaler]</a></em>
 			            <br />
 			        </p>
 			    </div>
@@ -39,8 +40,6 @@
 			?>
 
 		</section>
-
-	</div>
 
 </div>
 
