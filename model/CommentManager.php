924c883eb426f737ae9e95444df5f52de81746cf
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 14:18:43 +0100

    Update CommentManager Model with 2 new requests : postComment() & removeComment()

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -15,4 +15,24 @@ class CommentManager extends Manager
 
         return $comments;
     }
+
+    public function postComment($postId, $commentAuthor, $commentContent)
+    {
+        $db = $this->dbConnect();
+        $comments = $db->prepare('INSERT INTO comments(post_id, comment_author, comment_content, creation_date) VALUES(?, ?, ?, NOW())');
+        $newComment = $comments->execute(array($postId, $commentAuthor, $commentContent));
+
+        return $newComment;
+    }
+
+    // Fonction permettant la suppression du commentaire
+    public function removeComment($commentId)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('DELETE FROM comments WHERE id = ?');
+        $req->execute(array($commentId));
+        $delete = $req->fetch();
+
+        return $delete;
+    }
 }
