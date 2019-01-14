a28a0f8016e5e46962a2875ff807d0c95fd3b655
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 14:11:14 +0100

    Add some html tags : jquery scripts, bootstrap link and path for javascript file

--- view/template.php
+++ view/template.php
@@ -2,7 +2,12 @@
 <html>
    <head>
       <meta charset="utf-8" />
-      <link rel="stylesheet" href="public/css/style.css"/> 
+      <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="public/css/style.css"/>
+      <script type="text/javascript" src="public/js/main.js"></script>
+
       <title><?= $title ?></title>
    </head>
    <body>
