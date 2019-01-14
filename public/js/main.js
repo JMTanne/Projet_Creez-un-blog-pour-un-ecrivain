81d8deaa4c4645752d9ecfabec52139b38250260
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-14 14:31:42 +0100

    Add new controller for message session + new folder for JS 'Session Messages' script

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+jQuery(function($){
+
+	var alert = $('#alert');
+	if (alert.length > 0){
+		
+		// Fonction gérant la fermeture de l'alerte avec un delay
+		alert.hide().slideDown(600)/*.delay(3000).slideUp()*/;
+		
+		// Fonction gérant la fermeture de l'alerte avec le "x" close
+		alert.find('.close').click(function(e){
+			e.preventDefault();
+			alert.slideUp();
+		})
+	}
+});
