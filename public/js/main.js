00d65bb285d2017de446db653e3c5cc42569734a
Howi <tanne.jean.marie@gmail.com>
2019-01-27 13:13:31 +0100

    Add comments on style css et js files

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,18 @@
-jQuery(function($){
+jQuery(function($) {
 
-	var alert = $('#alert');
-	if (alert.length > 0){
-		
-		// Fonction gérant la fermeture de l'alerte avec un delay
-		alert.hide().slideDown(600)/*.delay(3000).slideUp()*/;
-		
-		// Fonction gérant la fermeture de l'alerte avec le "x" close
-		alert.find('.close').click(function(e){
-			e.preventDefault();
-			alert.slideUp();
-		})
-	}
+    var alert = $('#alert');
+    if (alert.length > 0) {
+        // Definition of alert message closing (delay)
+        alert.hide().slideDown(600);
+
+        // Closing alert message with click event on 'x'
+        alert.find('.close').click(function(e) {
+            e.preventDefault();
+            alert.slideUp();
+        })
+    }
 });
-
+// TinyMCE init on '#mytextarea' html tags id
 tinymce.init({
     selector: '#mytextarea'
-  });
+});
