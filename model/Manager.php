8c4438b81bad186c7d26b8a296600684e66376fd
Jean-Marie TANNE <jm.tanne@meetic-corp.com>
2019-01-29 09:01:54 +0100

    Modif database access

--- model/Manager.php
+++ model/Manager.php
@@ -11,7 +11,6 @@ class Manager
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
-        /*$db = new \PDO('mysql:host=db769942789.hosting-data.io;dbname=db769942789;charset=utf8', 'dbo769942789', 'Azertyu1/');*/
         return $db;
     }
 }
